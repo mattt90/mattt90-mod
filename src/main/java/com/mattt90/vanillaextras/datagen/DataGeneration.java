9b078fae9db3877e9294c2f81a478aad413f7731
Matthew Schmirler <mschmirler13@alumni.uwosh.edu>
2024-08-11 15:35:58 -0500

    feat: add loot table for test block drops

--- src/main/java/com/mattt90/vanillaextras/datagen/DataGeneration.java
+++ src/main/java/com/mattt90/vanillaextras/datagen/DataGeneration.java
@@ -2,7 +2,12 @@ package com.mattt90.vanillaextras.datagen;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraftforge.data.event.GatherDataEvent;
+
+import java.util.Collections;
+import java.util.List;
 
 public class DataGeneration {
 
@@ -15,5 +20,7 @@ public class DataGeneration {
         generator.addProvider(event.includeClient(), new VeLanguageProvider(packOutput, "en_us"));
 
         generator.addProvider(event.includeServer(), new VeRecipes(packOutput));
+        generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),
+                List.of(new LootTableProvider.SubProviderEntry(VeLootTables::new, LootContextParamSets.BLOCK))));
     }
 }
